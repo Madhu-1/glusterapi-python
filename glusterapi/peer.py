c81d95044749aaffa7b99ea3e7978b10eb3ac25a
Madhu Rajanna <mrajanna@redhat.com>
2018-06-08 09:44:25 +0530

    updated code for peer rest api's
    Co-authored-by Devyani Kota <devyanikota@gmail.com>
    Signed-off-by: Madhu Rajanna <mrajanna@redhat.com>

--- glusterapi/peer.py
+++ glusterapi/peer.py
@@ -1,33 +1,36 @@
+import json
+
 from common import BaseAPI
 
 
 class PeerApis(BaseAPI):
-    def peer_probe(self, host):
+    def peer_add(self, host, metadata=None, zone=""):
         """
-        Gluster Peer Probe
+        Gluster Peer add
 
         :param host: (string) Hostname or IP
+        :param metadata: (dict) host metadata
+        :param zone: (string) zone id
         :raises: GlusterApiError on failure
         """
-        pass
+        req = dict()
+        req['addresses'] = []
+        req['addresses'].append(host)
+        if metadata is None:
+            metadata = dict()
+        req['metadata'] = metadata
+        req['zone'] = zone
+        return self._handle_request(self._post, 201, "/v1/peers", json.dumps(req))
 
-    def peer_attach(self, host):
+    def peer_remove(self, peerid):
         """
-        Gluster Peer Attach
+        Gluster Peer remove
 
-        :param host: (string) Hostname or IP
+        :param peerid: (string) peerid returned from peer_add
         :raises: GlusterApiError on failure
         """
-        return self.peer_probe(host)
-
-    def peer_detach(self, host):
-        """
-        Gluster Peer Detach
-
-        :param host: (string) Hostname or IP
-        :raises: GlusterApiError on failure
-        """
-        pass
+        url = "/v1/peers/" + peerid
+        return self._handle_request(self._delete, 204, url, None)
 
     def peer_status(self):
         """
